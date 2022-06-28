3402c05a76bfd85e53e1573de32acb656ca7c149
rezzouq <mohammed.rezzouq@siway.ma>
2022-06-28 15:51:49 +0100

    ci(when using formik elements like <Field> we dont have to write attributes like handlechange and handleblur...):

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -14,3 +14,17 @@ export const basicScema = yup.object().shape({
     .oneOf([yup.ref("password"), null], "Password must match")
     .required("Required"),
 });
+
+export const advancedSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .required("Required"),
+  jobType: yup
+    .string()
+    .oneOf(["designer", "developer", "manager", "other"], "Invalid Job Type")
+    .required("Required"),
+  acceptedTos: yup
+    .boolean()
+    .oneOf([true], "Please accept the terms of service"),
+});
